9bbcb21fb88a047b473a9248461277fc569dc919
Furkan <furkanayik@outlook.com>
2024-03-23 19:05:41 +0300

    streamlit app done

--- README.md
+++ README.md
@@ -1,107 +1,59 @@
-# PROJECT_NAME
+# Podcast Summarizer
 
-## Overview
+Podcast Summarizer is a Streamlit application that summarizes podcast episodes into concise text snippets, providing users with quick insights into the content of each episode.
 
-The PROJECT_NAME Project is a machine learning project designed to PURPOSE_OF_PROJECT.
-In this project, I leverage the power of machine learning to assist PROBLEM_DOMAIN/DEFINITON
+## Table of Contents
 
-## Project Goals
+- [Description](#description)
+- [Project Structure](#project-structure)
+- [Dependencies](#dependencies)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
 
-The primary goals of this project are as follows:
+## Description
 
-```text
-1. Base goal of the ML Model: 
+This project aims to provide a convenient way for users to get summaries of podcast episodes, helping them decide which episodes to listen to based on their interests.
 
-2. How this ML model solved problem: Enable accurate and efficient diagnosis by automating the classification of histological samples. 
-This can assist pathologists and medical professionals in identifying abnormal or cancerous tissues.
+The project consists of a summarization module (`summarizer.py`) containing the logic for summarizing podcast episodes, and a Streamlit application (`app.py`) that provides a user-friendly interface for interacting with the summarization functionality.
 
-3. Data Augmentation and Preprocessing: Implement data augmentation techniques and preprocessing methods to improve model generalization and robustness. 
-This involves techniques like random rotations, flips, and resizing of images.
+## Dependencies
 
-4. Transfer Learning: Utilize transfer learning with pre-trained deep learning models, such as ResNet, to leverage their feature extraction capabilities and fine-tune them for histology classification.
+The project relies on the following Python libraries:
 
-5. Performance Metrics: Employ various performance metrics, including accuracy, precision, recall, F1 score, ROC curves, and precision-recall curves, to evaluate and measure the model's effectiveness.
+- streamlit
+- transformers
+- torch
+- youtube_transcript_api
 
-6. Experiment Tracking: Monitor and visualize model training and performance using tools like Weights and Biases (wandb) to gain insights into the training process and make informed decisions.
-```
+You can install these dependencies using the provided `requirements.txt` file.
 
-## Installation
+## Usage
 
-`Prerequisites`: `Conda` (Miniconda or Anaconda) installed.
+To run the Podcast Summarizer application, follow these steps:
 
-Follow these steps to set up your project environment using Conda:
+1. Clone the repository:
 
-1. Clone this repository:
+   ```bash
+   git clone https://github.com/your_username/podcast-summarizer.git
+   cd podcast-summarizer
+   ```
 
-```bash
-git clone https://github.com/frknayk/REPOSITORY_NAME.git
-cd REPOSITORY_NAME
-```
+    2. Install the dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
 
-2. Install project as python-package, necessary for path management
+3. Run the Streamlit application:
+    ```bash
+    python -m streamlit run app.py
+    ```
 
-```bash
-pip install -e .
-```
-
-3. Create a Conda environment and install the required dependencies:
-
-```bash
-conda env create -f environment.yml
-conda activate env_PROJECT
-```
-
-## Weights and Biases (wandb) Setup
-
-1. Install wandb:
-
-```bash
-pip install wandb
-```
-
-2. Log in to wandb:
-
-```bash
-wandb login
-```
-
-Follow the prompts to authorize wandb.
-
-## Simple Usage Guide
-
-```bash
-uvicorn app:app --host 0.0.0.0 --port 8000 --reload
-```
-
-1. Prepare your dataset and organize it as needed.
-
-2. Modify the configuration parameters in config.yaml to customize your experiment settings.
-
-3. Run the training script:
-
-```bash
-python train.py --config config.yaml
-```
-
-4. Monitor and visualize your experiment in the wandb dashboard:
-
-```text
-Access the wandb dashboard at https://wandb.ai/.
-Explore training metrics, logs, and visualizations in real-time.
-```
-
-## Directory Structure
-
-- data/: Dataset files or links to datasets.
-- models/: Model architectures and related utilities.
-- configs/: Configuration YAML files for datasets.
-- logs/ : Consists wandb/ and checkpoints/ folder belonging to experiment runs.
-- environment.yaml: Conda environment specification.
-- requirements.txt: Additional Python package requirements.
-- README.md: Project overview and setup instructions.
+4. Access the application in your web browser at http://localhost:8501.
 
 
-## TODO-List
+## Contributing
+Contributions to the Podcast Summarizer project are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
 
-- TODO_1
-- TODO_2
+## License
+This project is licensed under the MIT License.
--- app.py
+++ app.py
@@ -1,19 +1,163 @@
 import streamlit as st
-
-from frontend.page_main import page_main
-# from pages_app.page_testing import page_testing
-# from pages_app.page_training import page_training
-
-st.set_page_config(
-    page_title="User Interface of ML App",
-    page_icon="🕹️",
-    layout="wide",
+# Transcript
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
+# Summarization
+from transformers import (
+    pipeline,
+    AutoModelForSpeechSeq2Seq,
+    AutoProcessor,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
 )
+import torch
+import re
 
 
-page_names_to_funcs = {
-    "Main Page": page_main,
-}
+def fetch_transcript(video_url):
+    try:
+        # Extract the video ID from the URL
+        video_id = video_url.split("v=")[1]
+        # Fetch the transcript for the video
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        # Process the transcript data
+        text_transcript = "\n".join([entry['text'] for entry in transcript])
+        return text_transcript
+    except Exception as e:
+        return str(e)
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+def clean_transcript(transcript):
+    # Remove non-speech elements (e.g., laughter, background noises)
+    transcript = re.sub(r'\[.*?\]', '', transcript)
+
+    # Correct spelling and grammar (you can use libraries like NLTK or spaCy for this)
+    # Example:
+    # import nltk
+    # transcript = ' '.join(nltk.word_tokenize(transcript))
+
+    # Normalize punctuation and formatting
+    transcript = transcript.replace('\n', ' ')  # Remove line breaks
+    transcript = re.sub(r'\s+', ' ', transcript)  # Remove extra whitespaces
+
+    # Remove timestamps and annotations
+    transcript = re.sub(r'\[\d+:\d+:\d+\]', '', transcript)
+
+    # Handle speaker identification (if present)
+    # Example: transcript = re.sub(r'Speaker\d+:', '', transcript)
+
+    # Remove filler words and phrases
+    filler_words = ['like', 'you know', 'sort of']  # Add more as needed
+    for word in filler_words:
+        transcript = transcript.replace(word, '')
+    
+    # Replace common contractions with their expanded forms
+    transcript = transcript.replace("won't", "will not")
+    transcript = transcript.replace("can't", "cannot")
+    transcript = transcript.replace("n't", " not")
+    transcript = transcript.replace("'ll", " will")
+    transcript = transcript.replace("'ve", " have")
+    transcript = transcript.replace("'re", " are")
+    transcript = transcript.replace("'d", " would")
+    transcript = transcript.replace("'s", " is")
+
+    return transcript.strip()  # Trim leading/trailing whitespaces
+
+def extract_video_id(url):
+    """Extracts the YouTube video ID from the URL."""
+    match = re.search(r"(?<=v=)[\w-]+", url)
+    if match:
+        return match.group(0)
+    else:
+        return None
+
+
+def summarize_transcript(text, llama_pipeline):
+    def summarize_text(llama_pipeline, system_prompt, text):
+        # Format the input text with special tokens for the model
+        text = f"""
+        <s>[INST] <<SYS>>
+        {system_prompt}
+        <</SYS>>
+        {text}[/INST]
+        """
+        # Generate sequences using the pipeline with specified parameters
+        sequences = llama_pipeline(text)
+        # Extract the generated text from the sequences
+        generated_text = sequences[0]["generated_text"]
+        # Trim the generated text to remove the instruction part
+        generated_text = generated_text[generated_text.find('[/INST]')+len('[/INST]'):]
+        # Return the processed generated text
+        return generated_text
+    # Define the maximum input length for each iteration of summarization
+    input_len = 1000
+    # Start an infinite loop to repeatedly summarize the text
+    while True:
+        # Print the current length of the text
+        print(len(text))
+        # Call the chat function to summarize the text. Only the first 'input_len' characters are considered for summarization
+        summary = summarize_text(llama_pipeline, "", "Summarize the following: " + text[0:input_len])
+        if len(summary) < input_len:
+            return summary
+        # Concatenate the current summary with the remaining part of the text for the next iteration
+        text = summary + " " + text[input_len:]
+
+# Load the model and tokenizer
+@st.cache_resource()
+def load_model():
+    # Define the model name to be used for the chat function
+    model_name = "meta-llama/Llama-2-7b-chat-hf"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    pipeline_llama2 = pipeline(
+        "text-generation", #task
+        model=model_name,
+        tokenizer=tokenizer,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        device_map="auto",
+        # max_length=max_token_length,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id
+    )
+    return pipeline_llama2
+
+def main():
+    st.title("YouTube Video Preview")
+
+    with st.spinner('Loading checkpoint shards of LLAMA-2'):
+        pipeline_llama2 = load_model()
+    st.success('Done!')
+
+    # Input field for the YouTube video link
+    youtube_url = st.text_input("Paste YouTube Video Link:")
+    
+    # Extract video ID from the URL
+    video_id = extract_video_id(youtube_url)
+
+    # Display video preview if video ID is found
+    if video_id:
+        video_url = f"https://www.youtube.com/watch?v={video_id}"
+        st.video(video_url, format='video/mp4')
+        video_transcript = clean_transcript(fetch_transcript(video_url))
+        if video_transcript:
+            # Display transcript and summary side by side
+            col1, col2 = st.columns(2)
+            with col1:
+                st.subheader("Transcript:")
+                st.text_area(" ", video_transcript, height=400)
+
+            with col2:
+                st.subheader("Summary:")
+                video_summary = summarize_transcript(video_transcript, pipeline_llama2)
+                st.text_area(" ", video_summary, height=400)
+                print(f"Summary:{video_summary}")   
+        else:
+            st.error("Failed to fetch video transcript. Please check the video ID or try again later.")
+
+    elif youtube_url:
+        st.warning("Invalid YouTube Video Link")
+
+if __name__ == "__main__":
+    main()
